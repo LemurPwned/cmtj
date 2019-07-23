1e710340c0848dd7c3e7ec87987021bcabd3f34b
LemurPwned <jakub.mojsiejuk@gmail.com>
2019-07-23 20:49:38 +0200

    sketch of macromagnetic simulation

--- junction.py
+++ junction.py
@@ -28,20 +28,20 @@ class Layer:
         self.dipole_tensor = np.array(
             [[0.0173, 0.0, 0.0], [0.0, 0.0173, 0.0], [0.0, 0.0, -0.0345]], dtype=float)
 
-        self.params = ['m', 'anisotropy', 'Hext']
+        self.params = ['m', 'anisotropy', 'Hext', 'coupling']
         self.Hext = np.array([0.0, 0.0, 0.0])
         self.update_external_field = None
         self.update_anisotropy = None
         self.update_coupling = None
         self.log = []
 
-    def Heff(self, time, coupled_layers=None):
+    def Heff(self, time, coupled_layers):
         if self.update_anisotropy:
-            self.update_anisotropy(time)
+            self.anisotropy = self.update_anisotropy(time)
         if self.update_external_field:
             self.Hext = self.update_external_field(self.Hext, time)
         if self.update_coupling:
-            self.update_coupling(time)
+            self.coupling = self.update_coupling(time)
 
         heff = \
             self.Hext + \
@@ -64,8 +64,8 @@ class Layer:
         heff_iec = np.array([0, 0, 0])
         if not coupled_layers:
             return heff_iec
-        for coupled_m in coupled_layers:
-            heff_iec += self.coupling*coupled_m / \
+        for layer in coupled_layers:
+            heff_iec += self.coupling*layer.m / \
                 (constant.MAGNETIC_PERMEABILITY*self.Ms*self.thickness)
         return heff_iec
 
@@ -82,8 +82,8 @@ class Layer:
                 value_list.append(getattr(self, param))
         self.log.append(value_list)
 
-    def llg(self, time, m):
-        heff = self.Heff(time)
+    def llg(self, time, m, coupled_layers):
+        heff = self.Heff(time, coupled_layers)
         dmdt = -1*constant.GYRO * \
             np.cross(m, heff) - 1*constant.GYRO * \
             constant.DAMPING*np.cross(m, np.cross(m, heff))
@@ -95,21 +95,26 @@ class Layer:
             time = t
             stopping_cond = (t + dt - time_step)
             while time < stopping_cond:
-                m = self.m
-                k1 = time_step*self.llg(time, self.m)
-                k2 = time_step*self.llg(time + 0.5*time_step, self.m+0.5*k1)
-                k3 = time_step*self.llg(time + 0.5*time_step, self.m+0.5*k1)
-                k4 = time_step*self.llg(time + time_step, self.m+k3)
-                m = m + (k1 + 2.0*k2 + 2.0*k3 + k4)/6.0
-                m = m/np.linalg.norm(m)
-                # update m
-                self.m_history.append(self.m)
-                self.m = m
+                self.rk4_step(time, time_step)
                 time += time_step
             self.log_layer_parameters(t)
         pd.DataFrame(data=self.log, columns=['time', 'mx', 'my', 'mz',
                                              'ax', 'ay', 'az',
                                              'Hext_x', 'Hext_y', 'Hext_z']).to_csv('results.csv')
+
+    def rk4_step(self, time, time_step, coupled_layers=None):
+        m = self.m
+        k1 = time_step*self.llg(time, self.m, coupled_layers)
+        k2 = time_step*self.llg(time + 0.5*time_step,
+                                self.m+0.5*k1, coupled_layers)
+        k3 = time_step*self.llg(time + 0.5*time_step,
+                                self.m+0.5*k1, coupled_layers)
+        k4 = time_step*self.llg(time + time_step, self.m+k3, coupled_layers)
+        m = m + (k1 + 2.0*k2 + 2.0*k3 + k4)/6.0
+        m = m/np.linalg.norm(m)
+        # update m
+        self.m_history.append(self.m)
+        self.m = m
 
 
 def plot_results():
@@ -119,14 +124,59 @@ def plot_results():
     plt.show()
 
 
+class Junction():
+    def __init__(self):
+        self.layers = []
+        self.couplings = []
+        self.log = []
+        self.params = ['m', 'anisotropy', 'Hext', 'coupling']
+
+    def log_layer_parameters(self, time):
+        value_list = [time/1e-9]  # nanoseconds
+        for param in self.params:
+            for layer in self.layers:
+                pval = getattr(layer, param)
+                value_list.extend(pval)
+        self.log.append(value_list)
+
+    def run_simulation(self, stop_time, dt=1e-12, time_step=1e-13):
+        # LLG#
+        if not self.layers:
+            raise ValueError("No layers in junction!")
+        for t in np.arange(0, stop_time, dt):
+            time = t
+            stopping_cond = (t + dt - time_step)
+            while time < stopping_cond:
+                for i, layer in enumerate(self.layers):
+                    print(i, layer.id)
+                    coupled_layers = [self.layers[j]
+                                      for j in self.couplings[i]]
+                    layer.rk4_step(time, time_step, coupled_layers)
+            self.log_layer_parameters(t)
+
+        cols = []
+        for param in self.params:
+            for layer in self.layers:
+                for suffix in ['x', 'y', 'z']:
+                    cols.append(param+'_'+suffix+'_'+layer.id)
+        pd.DataFrame(data=self.log, columns=[
+                     'time', *cols]).to_csv('results.csv')
+
+
 if __name__ == "__main__":
+
+    junction = Junction()
     l1 = Layer(
         "free", [0, 0.5, 0.5], [0, 100e6, 0], 1200e3, 3e-9
     )
     l2 = Layer(
         "reference", [0, 0., 1.], [0, 0, 0], 1200e3, 3e-9
     )
+    juncton.layers = [l1, l2]
+    junction.couplings = [[1], [0]]
+    l2.update_external_field = lambda Hext, t: np.array(
+        [0, 20*np.sin(5e9*2*np.pi*t), 0.])
     l1.update_external_field = lambda Hext, t: np.array(
         [0, 20*np.sin(5e9*2*np.pi*t), 0.])
-    l1.run_simulation(1e-9)
-    plot_results()
+    junction.run_simulation(1e-9)
+    # plot_results()
