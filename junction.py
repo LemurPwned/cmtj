adding the sketch for vsd and some utility functions. Fixed old IEC formula

junction.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 from constants import Constants
 
 constant = Constants()
@@ -10,7 +11,9 @@ constant = Constants()
 class Layer:
     def __init__(self, id_, start_mag,
                  start_anisotropy, K, Ms, thickness):
+        # file written file id
         self.id = id_
+        # magnetisation options
         if start_mag is not np.array:
             start_mag = np.array(start_mag, dtype=float)
         self.m = start_mag
@@ -18,6 +21,8 @@ class Layer:
         self.m_history = []
         if start_anisotropy is not np.array:
             start_anisotropy = np.array(start_anisotropy, dtype=float)
+
+        # other parameters section
         self.anisotropy = start_anisotropy
         self.K = K
         self.Ms = Ms
@@ -29,6 +34,7 @@ class Layer:
         self.dipole_tensor = np.array(
             [[0.0173, 0.0, 0.0], [0.0, 0.0173, 0.0], [0.0, 0.0, -0.0345]], dtype=float)
 
+        # utility function
         self.params = ['m', 'anisotropy', 'Hext', 'coupling']
         self.Hext = np.array([0.0, 0.0, 0.0])
         self.update_external_field = None
@@ -46,11 +52,10 @@ class Layer:
             self.coupling = self.update_coupling(time)
 
         heff = \
-            self.Hext + self.calculate_anisotropy()+\
-                 self.calculate_demagnetisation_field()
-
-        # self.calculate_dipole_interaction()
-        # self.calculate_interlayer_exchange_coupling(coupled_layers)
+            self.Hext + self.calculate_anisotropy() +\
+            self.calculate_demagnetisation_field() +\
+            self.calculate_interlayer_exchange_coupling_field(coupled_layers) +\
+            self.calculate_dipole_interaction()
         return heff
 
     def calculate_dipole_interaction(self):
@@ -60,12 +65,25 @@ class Layer:
         nom = 2*self.K*np.dot(self.m, self.anisotropy)*self.anisotropy
         return nom/(constant.MAGNETIC_PERMEABILITY * self.Ms)
 
+    """
+    # this is the old formula that yields non-zero field 
+    # for parallel spins
     def calculate_interlayer_exchange_coupling(self, time, coupled_layers):
         heff_iec = np.array([0, 0., 0], dtype=float)
         if not coupled_layers:
             return heff_iec
         for layer in coupled_layers:
             heff_iec += self.coupling*layer.m / \
+                (constant.MAGNETIC_PERMEABILITY*self.Ms*self.thickness)
+        return heff_iec
+    """
+
+    def calculate_interlayer_exchange_coupling_field(self, coupled_layers):
+        heff_iec = np.array([0, 0., 0], dtype=float)
+        if not coupled_layers:
+            return heff_iec
+        for layer in coupled_layers:
+            heff_iec += self.coupling*(layer.m - self.m) / \
                 (constant.MAGNETIC_PERMEABILITY*self.Ms*self.thickness)
         return heff_iec
 
@@ -77,6 +95,27 @@ class Layer:
         dmdt = -1.0*constant.GYRO * \
             np.cross(m, heff) - 1.0*constant.GYRO * \
             constant.DAMPING*np.cross(m, np.cross(m, heff))
+        return dmdt
+
+    def llg_STT(self, time, m, coupled_layers, spin_polarized_layer):
+        """
+        :param spin_polarized_layer is a vector of a layer in which 
+            the current is polarised
+        """
+        heff = self.Heff(time, coupled_layers)
+        aj = (constant.HBAR*self.current_density *
+              self.spin_polarisation_efficiency)/(2*constant.ELECTRON_CHARGE *
+                                                  constant.MAGNETIC_PERMEABILITY *
+                                                  self.Ms*self.thickness)
+        bj = np.array([0., 0., 0.])
+        p_cross = np.cross(m, spin_polarized_layer)
+        stt_term = constant.GYRO*(aj*np.cross(m, p_cross) +
+                                  bj*p_cross)
+
+        dmdt = -1.0*constant.GYRO * \
+            np.cross(m, heff) - 1.0*constant.GYRO * \
+            constant.DAMPING*np.cross(m, np.cross(m, heff)) +\
+            stt_term
         return dmdt
 
     def rk4_step(self, time, time_step, coupled_layers=None):
@@ -94,11 +133,13 @@ class Layer:
 
 
 class Junction():
-    def __init__(self, layers, couplings):
+    def __init__(self, layers, couplings, persist=False):
         self.layers = layers
+        self.layers_id = {layer.id: layer for layer in layers}
         self.couplings = couplings
         self.log = []
         self.params = ['m', 'anisotropy', 'Hext']
+        self.persist_resultant_dataframe = persist  # read file only or memory leftover?
 
     def log_layer_parameters(self, time):
         value_list = [time/1e-9]  # nanoseconds
@@ -108,10 +149,21 @@ class Junction():
                 value_list.extend(pval)
         self.log.append(value_list)
 
+    def set_junction_global_external_field(self, constant_field_value):
+        for layer in self.layers:
+            layer.constant_external_field = constant_field_value
+
+    def restart(self):
+        # just revert to initial parameters
+        # that will involve reading the saved file and overrite the state
+        pass
+
     def run_simulation(self, stop_time, dt=1e-12, time_step=1e-13):
         # LLG#
         if not self.layers:
             raise ValueError("No layers in junction!")
+        print("Kicking off the simulation!")
+        tstart = time.time()
         if self.couplings:
             for t in np.arange(0, stop_time, dt):
                 time = t
@@ -131,13 +183,20 @@ class Junction():
                     layer.rk4_step(t, time_step)
                 self.log_layer_parameters(t)
 
+        tend = time.time()
+        print(
+            f"Simulation has finished in {tend-tstart}s. Writting the results...")
         cols = []
         for param in self.params:
             for layer in self.layers:
                 for suffix in ['x', 'y', 'z']:
                     cols.append(param+'_'+suffix+'_'+layer.id)
-        pd.DataFrame(data=self.log, columns=[
-                     'time', *cols]).to_csv('results.csv')
+        if self.persist_resultant_dataframe:
+            self.junction_result = pd.DataFrame(data=self.log, columns=[
+                'time', *cols]).to_csv('results.csv')
+        else:
+            pd.DataFrame(data=self.log, columns=[
+                'time', *cols]).to_csv('results.csv')
 
 
 def plot_results():