ddd0fbbb3599309e0461b132d8184090601f5488
LemurPwned <jakub.mojsiejuk@gmail.com>
2021-06-26 22:37:52 +0200

    adding new namespace util functions

---
+++ cmtj/utils/filters.py
@@ -0,0 +1,26 @@
+from scipy.signal import butter, lfilter
+
+
+class Filters:
+    @staticmethod
+    def butter_bandpass_filter(data, pass_freq, fs, order=5):
+        nyq = 0.5 * fs
+        if pass_freq == 0:
+            pass_freq = 0.1
+        try:
+            b, a = butter(order, [0.9 * pass_freq / nyq, pass_freq / nyq],
+                          btype='bandpass',
+                          analog=False)
+        except ValueError:
+            print(fs, pass_freq, nyq, 0.9 * pass_freq / nyq, pass_freq / nyq)
+            raise ValueError("Error in filtering")
+        y = lfilter(b, a, data, zi=None)
+        return y
+
+    @staticmethod
+    def butter_lowpass_filter(data, cutoff, fs, order=5):
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        y = lfilter(b, a, data, zi=None)
+        return y
---
+++ cmtj/utils/linear.py
@@ -0,0 +1,63 @@
+import numpy as np 
+from typing import Tuple
+
+class FieldScan:
+    @staticmethod
+    def _trig_compute(theta, phi) -> Tuple:
+        st = np.sin(np.deg2rad(theta))
+        ct = np.cos(np.deg2rad(theta))
+        sp = np.sin(np.deg2rad(phi))
+        cp = np.cos(np.deg2rad(phi))
+        return st, ct, sp, cp
+
+    @staticmethod
+    def amplitude_scan(start: float, stop: float, steps: int, theta: float, phi: float) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Compute a linear magnitude sweep.
+        :param start:
+        :param stop:
+        :param steps: 
+        :param theta:
+        :param phi:
+
+        :returns: linear amplitude, field vectors
+        """
+        Hspan = np.linspace(start, stop, endpoint=True, num=steps)
+        st, ct, sp, cp = FieldScan._trig_compute(theta, phi)
+        Hx = st*cp*Hspan
+        Hy = st*sp * Hspan
+        Hz = ct*Hspan
+        return Hspan, np.vstack((Hx, Hy, Hz))
+    
+    @staticmethod
+    def theta_scan(start: float, stop: float, steps: int, amplitude: float, phi: float) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Compute a linear theta angle sweep.
+        :param start:
+        :param stop:
+        :param steps: 
+        :param magnitude: magnitude of the scanned field
+        :param phi:
+        """
+        theta_span = np.linspace(start, stop, endpoint=True, num=steps)
+        st, ct, sp, cp = FieldScan._trig_compute(theta_span, phi)
+        Hx = st*cp*amplitude
+        Hy = st*sp * amplitude
+        Hz = ct*amplitude
+        return theta_span, np.vstack((Hx, Hy, Hz))
+
+    def phi_scan(start: float, stop: float, steps: int, amplitude: float, theta: float) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Compute a linear phi angle sweep.
+        :param start:
+        :param stop:
+        :param steps: 
+        :param magnitude: magnitude of the scanned field
+        :param theta:
+        """
+        phi_span = np.linspace(start, stop, endpoint=True, num=steps)
+        st, ct, sp, cp = FieldScan._trig_compute(theta, phi_span)
+        Hx = st*cp*amplitude
+        Hy = st*sp * amplitude
+        Hz = ct*amplitude
+        return phi_span, np.hstack((Hx, Hy, Hz))
