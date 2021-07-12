4bfb502f6b4665553a47371b63143e7f172de8b3
LemurPwned <jakub.mojsiejuk@gmail.com>
2021-07-12 14:32:53 +0200

    mp torque

--- cmtj/utils/filters.py
+++ cmtj/utils/filters.py
@@ -5,15 +5,20 @@ class Filters:
     @staticmethod
     def butter_bandpass_filter(data, pass_freq, fs, order=5):
         nyq = 0.5 * fs
-        if pass_freq == 0:
-            pass_freq = 0.1
-        try:
-            b, a = butter(order, [0.9 * pass_freq / nyq, pass_freq / nyq],
-                          btype='bandpass',
-                          analog=False)
-        except ValueError:
-            print(fs, pass_freq, nyq, 0.9 * pass_freq / nyq, pass_freq / nyq)
-            raise ValueError("Error in filtering")
+        if isinstance(pass_freq, float):
+            if pass_freq == 0:
+                pass_freq = 0.1
+                try:
+                    b, a = butter(order, [0.9 * pass_freq / nyq, pass_freq / nyq],
+                                btype='bandpass',
+                                analog=False)
+                except ValueError:
+                    print(fs, pass_freq, nyq, 0.9 * pass_freq / nyq, pass_freq / nyq)
+                    raise ValueError("Error in filtering")
+        elif isinstance(pass_freq, tuple):
+             b, a = butter(order, [pass_freq[0], pass_freq[1]],
+                                btype='bandpass',
+                                analog=False)
         y = lfilter(b, a, data, zi=None)
         return y
 
