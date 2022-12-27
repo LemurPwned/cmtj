65353a4d5d3ba3d7553d967e0002f4ec0fce8196
LemurPwned <jakub.mojsiejuk@gmail.com>
2022-12-27 14:23:19 +0100

    extending domain dynamics with dynamic current drivers

---
+++ cmtj/models/drivers.py
@@ -0,0 +1,22 @@
+import math
+
+from numba import njit
+
+
+def constant_driver(t, amp):
+    return amp
+
+
+@njit
+def sine_driver(t, amp, freq, phase):
+    return amp * math.sin(freq * t + phase)
+
+
+@njit
+def decay_driver(t, amp, tau):
+    return amp * math.exp(-t / tau)
+
+
+@njit
+def gaussian_driver(t, amp, sigma):
+    return amp * math.exp(-t**2 / sigma**2)
