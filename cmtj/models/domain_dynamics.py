d6b5ba54fd4f045328644c8cc018c44a79c4f2cf
LemurPwned <jakub.mojsiejuk@gmail.com>
2023-01-04 09:05:26 +0100

    docs update & init attempt at relativisitic DW dynamics

--- cmtj/models/domain_dynamics.py
+++ cmtj/models/domain_dynamics.py
@@ -12,11 +12,12 @@ from .sb import VectorObj
 gyro = gyromagnetic_ratio
 pi2 = math.pi / 2.
 """
-Change DMI sign for UP/DOWN domain wall (same chirality)
+Changing the DMI sign makes it an UP or DOWN domain wall (same chirality)
 """
 
 
 class DW:
+    """Initial conditions for the phi of DW equation."""
     NEEL_RIGHT = 0
     NEEL_LEFT = math.pi
     BLOCH_UP = math.pi / 2.
@@ -96,6 +97,33 @@ def compute_dynamics(X, phi, hx, hy, hz, alpha, dw, bj, beta, Hshe, je, Hdmi,
 
 @dataclass
 class DomainWallDynamics:
+    """Domain Wall dynamics class.
+    :param alpha: Gilbert damping.
+    :param domain_width: width of the domain.
+    :param H: applied magnetic field vector.
+    :param Hk: anisotropy field [A/m].
+    :param Ms: magnetisation saturation [A/m].
+    :param thickness: thickness of the FM material.
+    :param SHE_angle: Spin Hall Effect angle.
+    :param D: DMI constant.
+    :param beta: STT beta parameter.
+    :param p: STT polarisation efficiency.
+    :param V0_pin: pinning voltage constant.
+    :param pinning: the pinning period.
+    :param Lz: z-dimension of the FM block.
+    :param Ly: y-dimension of the FM block.
+    :param relativistic: whether to include relativistic effects?
+    :param vmax_magnon: m/s, max magnon velocity => 2A/Sd
+                        A -- exchange constant.
+                        S -- |S1| + |S2| = total spin density of sublattices.
+                        d -- interatomic distance.
+
+    For classical formulation see:
+    Current-driven dynamics of chiral ferromagnetic domain walls, Emori et al, 2013
+
+    For relativisitc formulation see:
+    Relativistic kinematics of a magnetic soliton, Caretta et al., 2020
+    """
     alpha: float
     domain_width: float
     H: VectorObj
@@ -110,9 +138,12 @@ class DomainWallDynamics:
     pinning: float = 30e-9
     Lz: float = 3e-9
     Ly: float = 120e-9
+    relativistic: bool = False
+    vmax_magnon: float = 5000  # m/s, max magnon velocity => 2A/Sd
 
     def __post_init__(self):
         self.dw = self.domain_width
+        self.dw_hat = self.dw
         denom = (2 * self.Ms * mu0 * echarge * self.thickness)
         self.Hshe = hbar * self.SHE_angle / denom
         self.Hdmi = self.D / (mu0 * self.Ms * self.dw)
@@ -127,34 +158,53 @@ class DomainWallDynamics:
         """
         self.je_driver = driver
 
-    def llg(self, t, vec):
+    def thiaville_llg(self, t, vec):
+        """Solve the Thiaville llg equation for LLG.
+        :param t: current simulation time.
+        :param vec: contains [X, phi], current DW position and its chirality.
+        :returns (dXdt, dPhidt): velocity and change of chirality.
+        """
         X, phi = vec
         je_at_t = self.je_driver(t=t)
-        return compute_dynamics(X,
-                                phi,
-                                hx=self.hx,
-                                hy=self.hy,
-                                hz=self.hz,
-                                alpha=self.alpha,
-                                dw=self.dw,
-                                bj=self.bj,
-                                beta=self.beta,
-                                Hshe=self.Hshe,
-                                je=je_at_t,
-                                Hdmi=self.Hdmi,
-                                Hk=self.Hk,
-                                Ms=self.Ms,
-                                V0_pin=self.V0_pin,
-                                pinning=self.pinning,
-                                Lz=self.Lz,
-                                Ly=self.Ly)
+
+        dXdt, dPhidt = compute_dynamics(X,
+                                        phi,
+                                        hx=self.hx,
+                                        hy=self.hy,
+                                        hz=self.hz,
+                                        alpha=self.alpha,
+                                        dw=self.dw_hat,
+                                        bj=self.bj,
+                                        beta=self.beta,
+                                        Hshe=self.Hshe,
+                                        je=je_at_t,
+                                        Hdmi=self.Hdmi,
+                                        Hk=self.Hk,
+                                        Ms=self.Ms,
+                                        V0_pin=self.V0_pin,
+                                        pinning=self.pinning,
+                                        Lz=self.Lz,
+                                        Ly=self.Ly)
+        if self.relativistic:
+            # contract the domain wall width
+            lorentz_factor = 1 / math.sqrt(1 - (self.vmax_magnon / dXdt)**2)
+            self.dw_hat = self.dw / lorentz_factor
+            self.Hdmi = self.D / (mu0 * self.Ms * self.dw_hat)
+
+        return dXdt, dPhidt
 
     def run(self,
             sim_time: float,
             x0: float = 0,
             phi0: float = 0,
             max_step=1e-10):
-        integrator = RK45(fun=self.llg,
+        """Run simulation of DW dynamics
+        :param sim_time: total simulation time (simulation units).
+        :param x0: starting position of the DW.
+        :param phi0: starting chiral angle of the DW.
+        :param max_step: maximum allowed step of the RK45 method.
+        """
+        integrator = RK45(fun=self.thiaville_llg,
                           t0=0.,
                           first_step=1e-16,
                           max_step=max_step,
