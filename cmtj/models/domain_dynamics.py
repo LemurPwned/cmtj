5b640ccda5175ca29958e209c1984db15416f22e
LemurPwned <jakub.mojsiejuk@gmail.com>
2023-01-29 12:39:53 +0100

    multilayer (bilayer) dynamics

--- cmtj/models/domain_dynamics.py
+++ cmtj/models/domain_dynamics.py
@@ -1,7 +1,7 @@
 import math
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, List
 
 from numba import njit
 from scipy.integrate import RK45
@@ -11,9 +11,6 @@ from .sb import VectorObj
 
 gyro = gyromagnetic_ratio
 pi2 = math.pi / 2.
-"""
-Changing the DMI sign makes it an UP or DOWN domain wall (same chirality)
-"""
 
 
 class DW:
@@ -59,9 +56,8 @@ def get_current_contributions(phi, bj, Hshe, je, alpha, beta, dw):
 
 
 @njit
-def compute_dynamics(X, phi, hx, hy, hz, alpha, dw, bj, beta, Hshe, je, Hdmi,
-                     Hk, Ms, V0_pin, pinning, Ly, Lz):
-    reduced_alpha = (1. + alpha**2)
+def compute_dynamics_(X, phi, hx, hy, hz, alpha, dw, bj, beta, Hshe, je, Hdmi,
+                      Hk, Ms, V0_pin, pinning, Ly, Lz):
     ag = alpha * gyro
     dg = dw * gyro * (math.pi / 2.)
     field_contr = get_field_contribution(X,
@@ -88,10 +84,42 @@ def compute_dynamics(X, phi, hx, hy, hz, alpha, dw, bj, beta, Hshe, je, Hdmi,
 
     dPhidt = ag * (Hk / 2.) * math.sin(2 * phi) - ag * pi2 * Hdmi * math.sin(
         phi) + field_contr[1] + j_contr[1]
+    return dXdt, dPhidt
 
-    dXdt = dXdt / reduced_alpha
-    dPhidt = dPhidt / reduced_alpha
 
+@njit
+def compute_gamma_a(X, phi, Q, dw, hk, hx, hy, hdmi, bj, IECterm):
+    pi2 = math.pi / 2.
+    fact_gamma = -0.5 * hk * math.sin(
+        2 * phi) + pi2 * hy * math.cos(phi) - pi2 * hx * math.sin(
+            phi) + Q * pi2 * hdmi * math.sin(phi) + IECterm
+    fact_stt = bj / dw
+    return gyro * fact_gamma + fact_stt
+
+
+@njit
+def compute_gamma_b(X, phi, Q, dw, hshe, hz, hr, beta, bj, Ms, Ly, Lz, V0_pin,
+                    pinning):
+    pi2 = math.pi / 2
+    hp = get_pinning_field(X,
+                           Ms=Ms,
+                           pinning=pinning,
+                           Ly=Ly,
+                           Lz=Lz,
+                           V0_pin=V0_pin)
+    fact_gamma = Q * (
+        hz + hp + pi2 * hshe * math.cos(phi)) - beta * pi2 * hr * math.cos(phi)
+    fact_stt = beta * bj / dw
+    return gyro * fact_gamma + fact_stt
+
+
+def compute_dynamics(X, phi, alpha, Q, dw, hx, hy, hz, hk, hdmi, hr, hshe,
+                     beta, bj, Ms, Ly, Lz, V0_pin, pinning, IECterm):
+    gamma_a = compute_gamma_a(X, phi, Q, dw, hk, hx, hy, hdmi, bj, IECterm)
+    gamma_b = compute_gamma_b(X, phi, Q, dw, hshe, hz, hr, beta, bj, Ms, Ly,
+                              Lz, V0_pin, pinning)
+    dXdt = dw * (gamma_a + alpha * gamma_b)
+    dPhidt = -alpha * gamma_a + gamma_b
     return dXdt, dPhidt
 
 
@@ -112,6 +140,8 @@ class DomainWallDynamics:
     :param pinning: the pinning period.
     :param Lz: z-dimension of the FM block.
     :param Ly: y-dimension of the FM block.
+    :param Q: up-down or down-up wall parameter (either 1 or -1).
+    :param Hr: Rashba field [A/m].
     :param relativistic: whether to include relativistic effects?
     :param vmax_magnon: m/s, max magnon velocity => 2A/Sd
                         A -- exchange constant.
@@ -138,6 +168,8 @@ class DomainWallDynamics:
     pinning: float = 30e-9
     Lz: float = 3e-9
     Ly: float = 120e-9
+    Q: int = 1
+    Hr: float = 0
     relativistic: bool = False
     vmax_magnon: float = 5000  # m/s, max magnon velocity => 2A/Sd
 
@@ -151,6 +183,7 @@ class DomainWallDynamics:
         self.bj = bohr_magneton * self.p / (echarge * self.Ms)
         self.hx, self.hy, self.hz = self.H.get_cartesian()
         self.je_driver = lambda t: 0
+        self.kappa = 1  # ratio of anisotropies, K = K/K0
 
     def set_current_function(self, driver: Callable):
         """
@@ -158,68 +191,101 @@ class DomainWallDynamics:
         """
         self.je_driver = driver
 
-    def thiaville_llg(self, t, vec):
+
+@dataclass
+class MultilayerWallDynamics:
+    layers: List[DomainWallDynamics]
+    J: float
+
+    def __post_init__(self):
+        if len(self.layers) > 2:
+            raise ValueError(
+                "BilayerWallDynamics only supports up to 2 layers")
+
+    def multilayer_dw_llg(self, t, vec):
         """Solve the Thiaville llg equation for LLG.
         :param t: current simulation time.
         :param vec: contains [X, phi], current DW position and its chirality.
         :returns (dXdt, dPhidt): velocity and change of chirality.
         """
-        X, phi = vec
-        je_at_t = self.je_driver(t=t)
+        # vector is X1, phi1, X2, phi2, ...
+        layer: DomainWallDynamics
+        new_vec = []
+        for i, layer in enumerate(self.layers):
+            je_at_t = layer.je_driver(t=t)
+            reduced_alpha = (1. + layer.alpha**2)
+            cphi = vec[(2 * i) + 1]
+            if len(self.layers) == 1:
+                Jterm = 0
+            else:
+                Jterm = 2 * self.J / (layer.Ms * mu0 * layer.thickness)
+                otherphi = vec[2 * (i - 1) + 1]
+                Jterm *= math.sin(cphi - otherphi)
 
-        dXdt, dPhidt = compute_dynamics(X,
-                                        phi,
-                                        hx=self.hx,
-                                        hy=self.hy,
-                                        hz=self.hz,
-                                        alpha=self.alpha,
-                                        dw=self.dw_hat,
-                                        bj=self.bj,
-                                        beta=self.beta,
-                                        Hshe=self.Hshe,
-                                        je=je_at_t,
-                                        Hdmi=self.Hdmi,
-                                        Hk=self.Hk,
-                                        Ms=self.Ms,
-                                        V0_pin=self.V0_pin,
-                                        pinning=self.pinning,
-                                        Lz=self.Lz,
-                                        Ly=self.Ly)
-        if self.relativistic:
-            # contract the domain wall width
-            lorentz_factor = 1 / math.sqrt(1 - (self.vmax_magnon / dXdt)**2)
-            self.dw_hat = self.dw / lorentz_factor
-            self.Hdmi = self.D / (mu0 * self.Ms * self.dw_hat)
-
-        return dXdt, dPhidt
+            dXdt, dPhidt = compute_dynamics(vec[2 * i],
+                                            phi=cphi,
+                                            Q=layer.Q,
+                                            hx=layer.hx,
+                                            hy=layer.hy,
+                                            hz=layer.hz,
+                                            alpha=layer.alpha,
+                                            dw=layer.dw_hat,
+                                            bj=layer.bj * je_at_t,
+                                            hr=layer.Hr,
+                                            beta=layer.beta,
+                                            hshe=layer.Hshe * je_at_t,
+                                            hdmi=layer.Hdmi,
+                                            hk=layer.Hk,
+                                            Ms=layer.Ms,
+                                            IECterm=Jterm,
+                                            V0_pin=layer.V0_pin,
+                                            pinning=layer.pinning,
+                                            Lz=layer.Lz,
+                                            Ly=layer.Ly)
+            dXdt = dXdt / reduced_alpha
+            dPhidt = dPhidt / reduced_alpha
+            # relaxation term for the domain wall width
+            layer.dw_hat = layer.dw / math.sqrt(1 + layer.kappa *
+                                                (math.sin(vec[2 * i + 1])**2))
+            if layer.relativistic:
+                # contract the domain wall width
+                lorentz_factor = 1 / math.sqrt(1 -
+                                               (layer.vmax_magnon / dXdt)**2)
+                layer.dw_hat = layer.dw / lorentz_factor
+            layer.Hdmi = layer.D / (mu0 * layer.Ms * layer.dw_hat)
+            new_vec.append(dXdt)
+            new_vec.append(dPhidt)
+        return new_vec
 
     def run(self,
             sim_time: float,
-            x0: float = 0,
-            phi0: float = 0,
-            max_step=1e-10):
+            starting_conditions: List[float],
+            max_step: float = 1e-10):
         """Run simulation of DW dynamics
         :param sim_time: total simulation time (simulation units).
         :param x0: starting position of the DW.
         :param phi0: starting chiral angle of the DW.
         :param max_step: maximum allowed step of the RK45 method.
         """
-        integrator = RK45(fun=self.thiaville_llg,
+        integrator = RK45(fun=self.multilayer_dw_llg,
                           t0=0.,
                           first_step=1e-16,
                           max_step=max_step,
-                          y0=[x0, phi0],
+                          y0=starting_conditions,
                           rtol=1e-12,
                           t_bound=sim_time)
         result = defaultdict(list)
         while True:
             integrator.step()
-            x, phi = integrator.y
-            vel = (x - integrator.y_old[0]) / integrator.step_size
+            layer_vecs = integrator.y
             result['t'].append(integrator.t)
-            result['v'].append(vel)
-            result['x'].append(x)
-            result['phi'].append(phi)
+            for i, layer in enumerate(self.layers):
+                x, phi = layer_vecs[2 * i], layer_vecs[2 * i + 1]
+                vel = (x - integrator.y_old[2 * i]) / integrator.step_size
+                result[f'dw_{i}'].append(layer.dw_hat)
+                result[f'v_{i}'].append(vel)
+                result[f'x_{i}'].append(x)
+                result[f'phi_{i}'].append(phi)
             if integrator.status == 'failed':
                 print("Failed to converge")
                 break
