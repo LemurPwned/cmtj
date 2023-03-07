52c140f04bd5f2b4da5dbd1ec9ab7ed56961f233
LemurPwned <jakub.mojsiejuk@gmail.com>
2023-03-07 20:51:04 +0100

    cleanup of dw dynamics, new relaxation methods

--- cmtj/models/domain_dynamics.py
+++ cmtj/models/domain_dynamics.py
@@ -1,7 +1,7 @@
 import math
 from abc import ABC
 from collections import defaultdict
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Callable, List, Literal
 
 from numba import njit
@@ -20,6 +20,12 @@ class DW:
     NEEL_LEFT = math.pi
     BLOCH_UP = math.pi / 2.
     BLOCH_DOWN = 3. * math.pi / 2.
+
+
+class DWRelax:
+    NO_RELAX = 0
+    STATIC = 1
+    DYNAMIC = 2
 
 
 @njit
@@ -56,47 +62,6 @@ def get_field_contribution(X, phi, hx, hy, hz, alpha, dw, Ms, V0_pin, pinning,
 
 
 @njit
-def get_current_contributions(phi, bj, Hshe, je, alpha, beta, dw):
-    bjj = bj * je
-    Hshej = Hshe * je * math.cos(phi)
-    dxdt = (1 + alpha * beta) * bjj + alpha * gyro * dw * pi2 * Hshej
-    dphidt = (beta - alpha) * (bjj / dw) + gyro * pi2 * Hshej
-    return dxdt, dphidt
-
-
-@njit
-def compute_dynamics_(X, phi, hx, hy, hz, alpha, dw, bj, beta, Hshe, je, Hdmi,
-                      Hk, Ms, V0_pin, pinning, Ly, Lz):
-    ag = alpha * gyro
-    dg = dw * gyro * (math.pi / 2.)
-    field_contr = get_field_contribution(X,
-                                         phi,
-                                         hx=hx,
-                                         hy=hy,
-                                         hz=hz,
-                                         alpha=alpha,
-                                         dw=dw,
-                                         Ms=Ms,
-                                         V0_pin=V0_pin,
-                                         pinning=pinning,
-                                         Ly=Ly,
-                                         Lz=Lz)
-    j_contr = get_current_contributions(phi,
-                                        bj=bj,
-                                        Hshe=Hshe,
-                                        je=je,
-                                        alpha=alpha,
-                                        beta=beta,
-                                        dw=dw)
-    dXdt = -gyro * dw * (Hk / 2.) * math.sin(
-        2 * phi) + dg * Hdmi * math.sin(phi) + field_contr[0] + j_contr[0]
-
-    dPhidt = ag * (Hk / 2.) * math.sin(2 * phi) - ag * pi2 * Hdmi * math.sin(
-        phi) + field_contr[1] + j_contr[1]
-    return dXdt, dPhidt
-
-
-@njit
 def compute_gamma_a(X, phi, Q, dw, hk, hx, hy, hdmi, bj, IECterm):
     pi2 = math.pi / 2.
     fact_gamma = -0.5 * hk * math.sin(
@@ -124,49 +89,47 @@ def compute_gamma_b(X, phi, Q, dw, hshe, hz, hr, beta, bj, Ms, Lx, Ly, Lz,
 
 
 @njit
-def compute_dynamics(X, phi, alpha, Q, dw, hx, hy, hz, hk, hdmi, hr, hshe,
+def compute_dynamics(X, phi, delta, alpha, Q, hx, hy, hz, hk, hdmi, hr, hshe,
                      beta, bj, Ms, Lx, Ly, Lz, V0_pin, V0_edge, pinning,
-                     IECterm):
-    gamma_a = compute_gamma_a(X, phi, Q, dw, hk, hx, hy, hdmi, bj, IECterm)
-    gamma_b = compute_gamma_b(X, phi, Q, dw, hshe, hz, hr, beta, bj, Ms, Lx,
+                     IECterm, thickness, A, Ku, Kp):
+    gamma_a = compute_gamma_a(X, phi, Q, delta, hk, hx, hy, hdmi, bj, IECterm)
+    gamma_b = compute_gamma_b(X, phi, Q, delta, hshe, hz, hr, beta, bj, Ms, Lx,
                               Ly, Lz, V0_pin, V0_edge, pinning)
-    dXdt = dw * (gamma_a + alpha * gamma_b)
+    dXdt = delta * (gamma_a + alpha * gamma_b)
     dPhidt = -alpha * gamma_a + gamma_b
-    return dXdt, dPhidt
+    pref = gyro / (alpha * mu0 * Ms * thickness)
+    # domain width relaxation from Thiaville
+    dDeltadt = pref * (A / delta - delta * (Ku + Kp * math.sin(phi)**2))
+    # dDeltadt  = 0
+    return dXdt, dPhidt, dDeltadt
 
 
 @dataclass
 class DomainWallDynamics:
     """Domain Wall dynamics class.
-    :param alpha: Gilbert damping.
-    :param domain_width: width of the domain.
     :param H: applied magnetic field vector.
-    :param Hk: anisotropy field [A/m].
+    :param alpha: Gilbert damping.
     :param Ms: magnetisation saturation [A/m].
     :param thickness: thickness of the FM material.
     :param SHE_angle: Spin Hall Effect angle.
     :param D: DMI constant.
+    :param Ku: perpendicular anisotropy constant.
+    :param Kp: inplane anisotropy constant.
+    :param A: exchange constant.
     :param beta: STT beta parameter.
     :param p: STT polarisation efficiency.
     :param V0_pin: pinning voltage constant.
+    :param V0_edge: edge voltage constant.
     :param pinning: the pinning period.
     :param Lx: z-dimension of the FM block.
     :param Ly: y-dimension of the FM block.
     :param Lz: z-dimension of the FM block.
     :param Q: up-down or down-up wall parameter (either 1 or -1).
     :param Hr: Rashba field [A/m].
-    :param kappa: ratio of anisotropies, K = K/K0, (try 0-1)
-    :param relativistic: whether to include relativistic effects?
-    :param vmax_magnon: m/s, max magnon velocity => 2A/Sd
-                        A -- exchange constant.
-                        S -- |S1| + |S2| = total spin density of sublattices.
-                        d -- interatomic distance.
-
+    :param moving_field: whether the anisotropy field is perpendicular or parallel
+    :param relax_dw: whether to relax the domain width. See DWRelax class.
     For classical formulation see:
     Current-driven dynamics of chiral ferromagnetic domain walls, Emori et al, 2013
-
-    For relativisitc formulation see:
-    Relativistic kinematics of a magnetic soliton, Caretta et al., 2020
     """
     H: VectorObj
     alpha: float
@@ -187,10 +150,11 @@ class DomainWallDynamics:
     Lz: float = 3e-9
     Q: int = 1
     Hr: float = 0
-    kappa: float = 1
     moving_field: Literal["perpendicular", "inplane"] = "perpendicular"
+    relax_dw: DWRelax = DWRelax.STATIC
+    dw0: float = field(init=False)
 
-    def __post__init__(self):
+    def __post_init__(self):
         # in post init we already have p
         self.bj = bohr_magneton * self.p / (echarge * self.Ms)
         self.je_driver = lambda t: 0
@@ -198,16 +162,20 @@ class DomainWallDynamics:
         self.Hshe = hbar * self.SHE_angle / denom
         self.hx, self.hy, self.hz = self.H.get_cartesian()
         self.dw0 = self.get_unrelaxed_domain_width()
+
         if self.moving_field == "perpendicular":
             self.Hk = self.get_perpendicular_anisotropy_field()
         elif self.moving_field == "inplane":
             self.Hk = self.get_inplane_anisotropy_field()
 
-    def get_unrelaxed_domain_width(self):
+    def get_unrelaxed_domain_width(self, effective=False):
         """Domain width is based off the effective perpendicular anisotropy.
         We reduce the perpendicular anisotropy by demagnetising field"""
-        # Keff = self.Ku - 0.5*mu0*(self.Ms/mu0)**2
-        Keff = self.Ku - (0.5 / mu0) * (self.Ms**2)
+        # Keff = self.Ku - 0.5*mu0*(self.Ms)**2
+        if effective:
+            Keff = self.Ku - (0.5 * mu0) * (self.Ms**2)
+        else:
+            Keff = self.Ku
         return math.sqrt(self.A / Keff)
 
     def set_current_function(self, driver: Callable):
@@ -237,8 +205,7 @@ class MultilayerWallDynamics:
 
     def __post_init__(self):
         if len(self.layers) > 2:
-            raise ValueError(
-                "BilayerWallDynamics only supports up to 2 layers")
+            raise ValueError("Wall dynamics supported up to 2 layers")
 
     def multilayer_dw_llg(self, t, vec):
         """Solve the Thiaville llg equation for LLG.
@@ -246,7 +213,7 @@ class MultilayerWallDynamics:
         :param vec: contains [X, phi, delta], current DW position, its angle and domain width.
         :returns (dXdt, dPhidt, dDeltad): velocity and change of angle and domain width.
         """
-        # vector is X1, phi1, X2, phi2, ...
+        # vector is X1, phi1, Delta1, X2, phi2, Delta2...
         layer: DomainWallDynamics
         new_vec = []
         for i, layer in enumerate(self.layers):
@@ -263,34 +230,37 @@ class MultilayerWallDynamics:
                 Jterm *= math.sin(lphi - otherphi)
 
             hdmi = layer.get_Hdmi(ldomain_width)
-            dXdt, dPhidt = compute_dynamics(lx,
-                                            phi=lphi,
-                                            Q=layer.Q,
-                                            hx=layer.hx,
-                                            hy=layer.hy,
-                                            hz=layer.hz,
-                                            alpha=layer.alpha,
-                                            dw=ldomain_width,
-                                            bj=layer.bj * je_at_t,
-                                            hr=layer.Hr,
-                                            beta=layer.beta,
-                                            hshe=layer.Hshe * je_at_t,
-                                            hdmi=hdmi,
-                                            hk=layer.Hk,
-                                            Ms=layer.Ms,
-                                            IECterm=Jterm,
-                                            V0_pin=layer.V0_pin,
-                                            V0_edge=layer.V0_edge,
-                                            pinning=layer.pinning,
-                                            Lx=layer.Lx,
-                                            Ly=layer.Ly,
-                                            Lz=layer.Lz)
+            dXdt, dPhidt, dDeltadt = compute_dynamics(
+                X=lx,
+                phi=lphi,
+                delta=ldomain_width,
+                Q=layer.Q,
+                hx=layer.hx,
+                hy=layer.hy,
+                hz=layer.hz,
+                alpha=layer.alpha,
+                bj=layer.bj * je_at_t,
+                hr=layer.Hr,
+                beta=layer.beta,
+                hshe=layer.Hshe * je_at_t,
+                hdmi=hdmi,
+                hk=layer.Hk,
+                Ms=layer.Ms,
+                IECterm=Jterm,
+                V0_pin=layer.V0_pin,
+                V0_edge=layer.V0_edge,
+                pinning=layer.pinning,
+                Lx=layer.Lx,
+                Ly=layer.Ly,
+                Lz=layer.Lz,
+                A=layer.A,
+                Ku=layer.Ku,
+                Kp=layer.Kp,
+                thickness=layer.thickness)
             dXdt = dXdt / reduced_alpha
             dPhidt = dPhidt / reduced_alpha
-            pref = gyro / (layer.alpha * mu0 * layer.Ms * layer.thickness)
-            # domain width relaxation from Thiaville
-            dDeltadt = pref * (layer.A / ldomain_width - ldomain_width *
-                               (layer.Ku + layer.Kp * math.sin(lphi)**2))
+            if layer.relax_dw != DWRelax.DYNAMIC:
+                dDeltadt = 0  # no relaxation in the ODE
             new_vec.extend([dXdt, dPhidt, dDeltadt])
         return new_vec
 
@@ -298,7 +268,7 @@ class MultilayerWallDynamics:
             sim_time: float,
             starting_conditions: List[float],
             max_step: float = 1e-10):
-        """Run simulation of DW dynamics
+        """Run simulation of DW dynamics.
         :param sim_time: total simulation time (simulation units).
         :param starting_conditions: starting position and angle of the DW.
         :param max_step: maximum allowed step of the RK45 method.
@@ -318,10 +288,14 @@ class MultilayerWallDynamics:
                 break
             layer_vecs = integrator.y
             result['t'].append(integrator.t)
-            for i, _ in enumerate(self.layers):
+            for i, layer in enumerate(self.layers):
                 x, phi, dw = layer_vecs[self.vector_size * i], layer_vecs[
                     self.vector_size * i +
                     1], layer_vecs[self.vector_size * i + 2]
+                # static relaxation Thiaville
+                if layer.relax_dw == DWRelax.STATIC:
+                    ratio = layer.Kp / layer.Ku
+                    dw = layer.dw0 / math.sqrt(1 + ratio * math.sin(phi)**2)
                 vel = (x - integrator.y_old[2 * i]) / integrator.step_size
                 result[f'dw_{i}'].append(dw)
                 result[f'v_{i}'].append(vel)
