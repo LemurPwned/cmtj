b364bcbb92ac305a57d7899352c84c26a431b3ef
LemurPwned <jakub.mojsiejuk@gmail.com>
2024-08-24 11:50:56 +0200

    updates to UI stuff

--- docker/Dockerfile.app
+++ docker/Dockerfile.app
@@ -3,10 +3,11 @@ EXPOSE 8501
 
 WORKDIR /app
 COPY view/ /app/
+COPY assets/ /app/
 RUN ls
 RUN python3 -m pip install streamlit && \
     git clone https://github.com/LemurPwned/cmtj.git && \
     cd cmtj && \
     python3 -m pip install .[utils]
 
-CMD streamlit run streamlit_app.py
+CMD streamlit run nav.py
--- view/nav.py
+++ view/nav.py
@@ -1,14 +1,27 @@
 import streamlit as st
+from PIL import Image
 
 app_title = "CMTJ UI"
-
-domain_page = st.Page("domain.py", title="Domain fitting", url_path="domains", icon="📈")
+im = Image.open("assets/icon-larger.png")
+st.logo(im)
+domain_page = st.Page(
+    "domain.py",
+    title="Domain fitting",
+    url_path="domains",
+    icon=":material/bubble_chart:",
+)
 spectrum_page = st.Page(
-    "streamlit_app.py", title="Spectrum fitting", url_path="spectrum", icon="📈"
+    "streamlit_app.py",
+    title="Spectrum fitting",
+    url_path="spectrum",
+    icon=":material/bar_chart:",
 )
 
 pg = st.navigation([domain_page, spectrum_page])
-
-st.set_page_config(page_title=app_title, page_icon=":rocket:")
+st.set_page_config(
+    page_title=app_title,
+    page_icon="🦈",
+    layout="wide",
+)
 pg.run()
 # > streamlit run ./view/streamlit_app.py --server.runOnSave true
